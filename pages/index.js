b4c7498d50714b95faeb6a2d74a7292605ea7895
Alex-tq <alextq3036@gmail.com>
2021-11-28 13:07:22 -0500

    Created a global.scss file and imported it to _app.js, installed sass

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@
 export default function HTMLModElement(){
     return (
         <div>
-            <h1>Hello from custom next</h1>
+            <h1>Hello from manual next</h1>
         </div>
     )
 }
